3b72550e1970306de3fb4e9949b2586ef59d49db
gaetan <gaetan.depin@epitech.eu>
2022-06-02 11:22:08 +0200

    change form validation lib

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 
 import { Link } from "react-router-dom";
 import AuthContainer from "components/auth-container/AuthContainer";
-import { FormControlLabel, Typography, Button, Checkbox, FormGroup, Box } from "@mui/material";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { FormControlLabel, Typography, Button, Checkbox, FormGroup, Box, TextField } from "@mui/material";
+import * as ReactForm from "react-hook-form";
 
 import { ReactComponent as Logo } from "icons/list-rocket.svg";
 
 function Login() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [rememberMe, setRememberMe] = React.useState(false);
+  const { register, handleSubmit, formState: { errors } } = ReactForm.useForm();
 
-  const handleSubmit = (e) => {
-    console.log("OK");
+  const onSubmit = (data) => {
+    console.log(data);
   }
 
   return (
@@ -22,43 +20,40 @@ function Login() {
         <Logo id="list-rocket-logo" />
       </Box>
       <Typography align="center" variant="h4">List Rocket</Typography>
-      <ValidatorForm
-        onSubmit={handleSubmit}
-        onError={errors => console.log(errors)}
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FormGroup>
-          <TextValidator
+          <TextField
             label="Email address"
             margin="dense"
             variant="standard"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            validators={["required", "isEmail"]}
-            errorMessages={["Email is required", "Email is not valid"]}
-            sx={{width: "100%"}}
+            {...register("email", {
+              required: "Email is required",
+              pattern: {value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, message: "Invalid email format"},
+              maxLength: {value: 120, message: "Email must be less than 120 characters"},
+            })}
+            helperText={errors.email !== undefined ? errors.email.message : ""}
+            error={errors.email !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextValidator
+          <TextField
             label="Password"
             type="password"
             margin="dense"
             variant="standard"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            validators={["required"]}
-            errorMessages={["Password is required"]}
-            sx={{width: "100%"}}
+            {...register("password", {required: "Password is required"})}
+            helperText={errors.password !== undefined ? errors.email.message : ""}
+            error={errors.password !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
           <Typography align="right" variant="body2"><Link to="/reset-password">Forgot your password ?</Link></Typography>
-          <FormControlLabel control={<Checkbox value={rememberMe} onChange={(_) => setRememberMe(!rememberMe)} />} label="Remember me" />
+          <FormControlLabel control={<Checkbox {...register("rememberMe")}/>} label="Remember me" />
           <Button type="submit" variant="contained">Login</Button>
         </FormGroup>
-      </ValidatorForm>
+      </form>
       <Typography align="center" variant="body2">You don't have an account ? <Link to="/register">Register</Link></Typography>
     </AuthContainer>
   );
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,21 +2,17 @@ import * as React from "react";
 
 import { Link } from "react-router-dom";
 import AuthContainer from "components/auth-container/AuthContainer";
-import { FormControlLabel, Typography, Button, Box, FormGroup } from "@mui/material";
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
+import { FormControlLabel, Typography, Button, Box, FormGroup, TextField } from "@mui/material";
+import * as ReactForm from "react-hook-form";
 
 import { ReactComponent as Logo } from "icons/list-rocket.svg";
 
 function Register() {
-  const [username, setUsername] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [repeatPassword, setRepeatPassword] = React.useState("");
-  const [birthDate, setBirthDate] = React.useState("");
-  const [phoneNumber, setPhoneNumber] = React.useState("");
+  const { register, watch, handleSubmit, formState: { errors } } = ReactForm.useForm();
+  const password = watch("password", "");
 
-  const handleSubmit = (e) => {
-    console.log("OK");
+  const onSubmit = (data) => {
+    console.log(data);
   }
 
   return (
@@ -25,97 +21,100 @@ function Register() {
         <Logo id="list-rocket-logo" />
       </Box>
       <Typography align="center" variant="h4">List Rocket</Typography>
-      <ValidatorForm
-        onSubmit={handleSubmit}
-        onError={errors => console.log(errors)}
-      >
+      <form onSubmit={handleSubmit(onSubmit)}>
         <FormGroup>
-          <TextValidator
+          <TextField
             label="Username"
             margin="dense"
             variant="standard"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            validators={["required"]}
-            errorMessages={["Username is required"]}
-            sx={{width: "100%"}}
+            required
+            {...register("username", {
+              required: "Username is required",
+              minLength: {value: 4, message: "Username must between 4 and 16 characters"},
+              maxLength: {value: 16, message: "Username must between 4 and 16 characters"},
+            })}
+            helperText={errors.username !== undefined ? errors.username.message : ""}
+            error={errors.username !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextValidator
+          <TextField
             label="Email address"
             margin="dense"
             variant="standard"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            validators={["required", "isEmail"]}
-            errorMessages={["Email is required", "Email is not valid"]}
-            sx={{width: "100%"}}
+            required
+            {...register("email", {
+              required: "Email is required",
+              pattern: {value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/, message: "Invalid email format"},
+              maxLength: {value: 120, message: "Email must be less than 120 characters"},
+            })}
+            helperText={errors.email !== undefined ? errors.email.message : ""}
+            error={errors.email !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextValidator
+          <TextField
             label="Password"
             type="password"
             margin="dense"
             variant="standard"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            validators={["required"]}
-            errorMessages={["Password is required"]}
-            sx={{width: "100%"}}
+            required
+            {...register("password", {
+              required: "Password is required",
+              maxLength: {value: 40, message: "Password must between 8 and 40 characters"},
+              minLength: {value: 8, message: "Password must between 8 and 40 characters"},
+            })}
+            helperText={errors.password !== undefined ? errors.email.message : ""}
+            error={errors.password !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextValidator
+          <TextField
             label="Confim password"
             type="password"
             margin="dense"
             variant="standard"
-            value={repeatPassword}
-            onChange={(e) => setRepeatPassword(e.target.value)}
-            validators={["required"]}
-            errorMessages={["Password is required"]}
-            sx={{width: "100%"}}
+            required
+            {...register("confirmPassword", {
+              validate: {match: (value) => value === password || "Password don't match"}
+            })}
+            helperText={errors.confirmPassword !== undefined ? errors.confirmPassword.message : ""}
+            error={errors.confirmPassword !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextValidator
+          <TextField
             label="Birth date"
             type="date"
             margin="dense"
             variant="standard"
-            value={birthDate}
-            onChange={(e) => setBirthDate(e.target.value)}
-            validators={["required"]}
-            errorMessages={["Birth date is required"]}
-            sx={{width: "100%"}}
+            required
+            {...register("birthDate")}
+            helperText={errors.password !== undefined ? errors.email.message : ""}
+            error={errors.password !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
-          <TextValidator
+          <TextField
             label="Phone number"
             margin="dense"
             type="tel"
             variant="standard"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            validators={["required"]}
-            errorMessages={["Phone number is required"]}
-            sx={{width: "100%"}}
+            {...register("phoneNumber")}
+            helperText={errors.password !== undefined ? errors.email.message : ""}
+            error={errors.password !== undefined ? true : false}
             InputLabelProps={{
               shrink: true,
             }}
           />
           <Button type="submit" variant="contained">Register</Button>
         </FormGroup>
-      </ValidatorForm>
+      </form>
       <Typography align="center" variant="body2">Already have an account ? <Link to="/login">Login</Link></Typography>
     </AuthContainer>
   );
